f2f887e39cd1bda50e2266bee16f428799da8200
Sher <38057955+khasherr@users.noreply.github.com>
2020-02-12 12:42:11 -0500

    sher changes v1
    andriod assignment 1

--- CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ CalculatorApp/app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -1,68 +1,105 @@
 package com.example.calculatorapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ArrayAdapter
+import android.view.View
+import android.widget.AdapterView
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
-import org.w3c.dom.Text
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var amountString: String //amountString does not need to initialized
+    private var tipAmount: Double = 0.0
+    private var actualTipAmount:Double = 0.0
+    private var totalAmount: Double = 0.0
+    private var perPerson: Double = 0.0
+    private var tipValue: Double = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var tipTotal = findViewById<TextView>(R.id.tipTotal)
-        var total = findViewById<TextView>(R.id.totalLb)
-        var numppl = findViewById<TextView>(R.id.perPerson)
 
-         var amount = findViewById<TextView>(R.id.amountValue)
-         var tip = findViewById<Spinner>(R.id.tipSpinner)
-//        ArrayAdapter.createFromResource(
-//            this,
-//            R.array.Tip_Percent
-//        )
+        var otherTipValueET = findViewById<TextView>(R.id.othertipValue)
+        var tipTotalTV = findViewById<TextView>(R.id.tipTotal)
+        var totalTV = findViewById<TextView>(R.id.totalLb)
+        var numpplTV = findViewById<TextView>(R.id.perPerson)
+        var amountET = findViewById<TextView>(R.id.amountValue)
+        var tipSpinner = findViewById<Spinner>(R.id.tipSpinner)
+        var numofpplSpinner = findViewById<Spinner>(R.id.numOfPplValue)
+        var clearButton = findViewById<Button>(R.id.clear)
+        var calculateTotalButton = findViewById<Button>(R.id.calc)
 
-         var tipValue = findViewById<TextView>(R.id.othertipValue)
-             tipValue.setEnabled(false)
+        otherTipValueET.isEnabled = false
 
-        var numofppl = findViewById<Spinner>(R.id.numOfPplValue)
+        if (tipSpinner != null) {
+            tipSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    otherTipValueET.isEnabled = tipSpinner.selectedItem.toString() == "Other"
+                }
 
-        var clear = findViewById<Button>(R.id.clear)
-        var calculateTotal = findViewById<Button>(R.id.calc)
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
 
-        calculateTotal.setOnClickListener(){
+        /*if (numofpplSpinner != null) {
+            numofpplSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                }
 
-            var amountString: String = (amount.text).toString();
-            var tipamount:Double = amountString.toDouble() * (tip.getSelectedItem().toString()).toDouble();
-            tipTotal.text = "Tip Total: " + ".2f".format(tipamount);
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }*/
 
-            var totalAmount:Double = amountString.toDouble() + tipamount;
-            total.text = "Amount Total: " + ".2f".format(totalAmount);
+        clearButton.setOnClickListener {
+            amountET.text = ""
+            otherTipValueET.text = ""
 
-            var perPerson:Double =totalAmount/(numofppl.getSelectedItem().toString()).toInt();
+            tipTotalTV.text = ""
+            totalTV.text = ""
+            numpplTV.text = ""
+            tipSpinner.setSelection(0)
+            numofpplSpinner.setSelection(0)
+        }
 
-            var s = (numofppl.getSelectedItem().toString()).toInt()
-            if (s > 1){
-               numppl.text = "Per Person: " + ".2f".format(perPerson);
+        calculateTotalButton.setOnClickListener {
+            amountString = (amountET.text).toString() //Get amount entered
+            if (tipSpinner.selectedItem.toString() == "Other") {
+                tipValue = ((otherTipValueET.text).toString()).toDouble()
+            } else {
+                tipValue = (tipSpinner.selectedItem.toString()).toDouble()
             }
 
-
+            tipAmount = amountString.toDouble() * (tipValue).toDouble()
+            actualTipAmount = (tipAmount / 100)
+            tipTotalTV.text = "Tip Total: " + actualTipAmount + ""
+            totalAmount = amountString.toDouble() + actualTipAmount
+            totalTV.text = "Amount Total: " + totalAmount + ""
+            perPerson = totalAmount / (numofpplSpinner.selectedItem.toString()).toInt()
+            var s = (numofpplSpinner.selectedItem.toString()).toInt()
+            if (s > 1) {
+                numpplTV.text = "Per Person: " + perPerson
+            } else {
+                // Do nothing
+            }
         }
 
-        clear.setOnClickListener(){
-            amount.text = "";
-            tipValue.text = "";
-
-            tipTotal.text = "";
-            total.text = "";
-
-        }
 
     }
-
 
 
 }
